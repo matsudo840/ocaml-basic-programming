8ff5123aea0f3b0a534e2fe40ebe34e337effcb1
matsudo840 <ryohhhhh@gmail.com>
2025-01-05 16:44:25 +0900

    5.6 fix logic

--- 5.6-kyosuukai.ml
+++ 5.6-kyosuukai.ml
@@ -5,8 +5,8 @@ let hanbetsushiki a b c = b ** 2. -. 4. *. a *. c
 (* 目的：2次方程式 ax2 + bx + c = 0 の係数 a, b, c を与えられたら、この2次方程式が虚数解を持つかどうかを判定する *)
 (* kyosuukai : float -> floab -> float -> bool *)
 let kyosuukai a b c = 
-  if hanbetsushiki a b c < 0.0 then true
-  else false
+  hanbetsushiki a b c < 0.0
+
 
 (* テスト *)
 let test1 = kyosuukai 0.0 0.0 0.0 = false
