4a6f29be6e0fabf0cdc8f6e5f36949fc15289742
matsudo840 <ryohhhhh@gmail.com>
2025-01-06 21:05:27 +0900

    Fix comment

--- 7.2-seiseki.ml
+++ 7.2-seiseki.ml
@@ -1,5 +1,5 @@
 (* 目的： 名前と成績の組を受け取り、成績を通知する文を表示する *)
-(* seiseki : (string * string) -> string *)
+(* seiseki : string * string -> string *)
 let seiseki pair = match pair with
   (a, b) -> a ^ "さんの成績は" ^ b ^ "です。"
 
