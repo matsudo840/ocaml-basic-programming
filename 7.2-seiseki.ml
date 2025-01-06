75859b1f6a6b2e7ac0dec27c46b5193a2ca6958a
matsudo840 <ryohhhhh@gmail.com>
2025-01-06 21:03:25 +0900

    7.2-seiseki

---
+++ 7.2-seiseki.ml
@@ -0,0 +1,9 @@
+(* 目的： 名前と成績の組を受け取り、成績を通知する文を表示する *)
+(* seiseki : (string * string) -> string *)
+let seiseki pair = match pair with
+  (a, b) -> a ^ "さんの成績は" ^ b ^ "です。"
+
+(* テスト *)
+let test1 =  seiseki ("A", "優") = "Aさんの成績は優です。"
+let test2 =  seiseki ("B", "良") = "Bさんの成績は良です。"
+let test3 =  seiseki ("C", "可") = "Cさんの成績は可です。"
